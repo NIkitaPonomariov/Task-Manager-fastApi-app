3cd233356277f0ae684434a33748811d9b32d63c
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-03 19:04:33 +0200

    create models in postgresql, write asynccontextmanager(lifespan), func get db, task orm to model rewrite get end point

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI, status
+from fastapi import Depends, FastAPI, status
 from pydantic import BaseModel
 from uuid import uuid4
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, sessionmaker 
 
-app = FastAPI()
+DATABASE_URL = "postgresql+psycopg://postgres:admin@127.0.0.1:15432/postgres"
+engine = create_engine(DATABASE_URL)
+Sessionlocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid4()))
+
+class TaskORM(Base):
+    __tablename__="tasks"
+    title: Mapped[str]
+    completed: Mapped[bool] = mapped_column(default=False)
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan = lifespan)
 
 
 app.add_middleware(
@@ -32,13 +53,27 @@ class TaskUpdateSchema(BaseModel):
 tasks: list[TaskSchema] = []
 
 
+def get_db():
+    db = Sessionlocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def task_orm_to_model(task_orm: TaskORM) -> TaskSchema:
+    return TaskSchema(id = task_orm.id, title = task_orm.title, completed = task_orm.completed)
+
+
 @app.get("/tasks")
-def tasks_reader() -> list[TaskSchema]:
-    return tasks
+def tasks_reader(db: Session = Depends(get_db)) -> list[TaskSchema]:
+    task_from_db = db.scalars(select(TaskORM)).all()
+    return [task_orm_to_model(task) for task in task_from_db]
 
 
 @app.post("/tasks", status_code = status.HTTP_201_CREATED)
-def create_task(payload: TaskCreateSchema) -> TaskSchema:
+def create_task(payload: TaskCreateSchema, db: Session = Depends(get_db)) -> TaskSchema:
     new_task = TaskSchema(id=str(uuid4()), title=payload.title,completed=False)
 
     tasks.append(new_task)
@@ -46,7 +81,7 @@ def create_task(payload: TaskCreateSchema) -> TaskSchema:
 
 
 @app.patch("/tasks/{task_id}")
-def update_task(task_id: str, payload: TaskUpdateSchema) -> TaskUpdateSchema:
+def update_task(task_id: str, payload: TaskUpdateSchema, db: Session = Depends(get_db)) -> TaskUpdateSchema:
     for task in tasks:
         if task.id == task_id:
             task.title = payload.title if payload.title else task.title
@@ -59,3 +94,11 @@ def delete_task(task_id):
     for task in tasks:
         if task.id == task_id:
             tasks.remove(task)
+
+
+
+
+# 1.13.37 <- tommorow start from here 
+# create models and code for collection 
+# end this project , made refactoring 
+# puth everything in docker and deploy
