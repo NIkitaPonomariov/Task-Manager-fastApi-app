4ec9357a306e55eef804fb233635281bd376ea17
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-11 15:15:33 +0200

    start write enpoinds for categories

--- backend/api/dependencies.py
+++ backend/api/dependencies.py
@@ -1,7 +1,11 @@
 from backend.services.task import TaskService
+from backend.services.category import CategoryService
 from sqlalchemy.orm import Session
 from backend.db.session import get_db
 from fastapi import Depends
 
 def get_task_servises(db: Session = Depends(get_db)):
     return TaskService(db)
+
+def get_category_services(db: Session = Depends(get_db)):
+    return CategoryService(db)
--- backend/api/routers/categories.py
+++ backend/api/routers/categories.py
@@ -1,12 +1,51 @@
-from http.client import HTTPException
-
-from fastapi import APIRouter, status, Depends
-from backend.schemas.task import TaskCreateSchema, TaskSchema, TaskUpdateSchema
-from backend.services.task import TaskService, TaskNotFound
-from backend.api.dependencies import get_task_servises
+from fastapi import APIRouter, status, Depends, HTTPException
+from backend.schemas.category import (
+    CategoryCreateSchema,
+    CategorySchema,
+    CategoryUpdateSchema,
+)
+from backend.services.category import CategoryService, CategoryNotFound
+from backend.api.dependencies import get_category_services
 
 router = APIRouter(prefix="/categories")
 
+@router.get("/")
+def category_reader(
+    category_service: CategoryService = Depends(get_category_services)
+) -> list[CategorySchema]:
+    return category_service.get_categories()
 
-#here i write all for categories!!!
+"""
 
+@router.post("", status_code = status.HTTP_201_CREATED)
+def create_task(
+    payload: TaskCreateSchema,
+    task_service: TaskService = Depends(get_task_servises)
+    ) -> TaskSchema:
+
+    return task_service.create_task(task_create=payload)
+
+@router.patch("/{task_id}")
+def update_task(
+    task_id: str,
+    payload: TaskUpdateSchema,
+    task_service: TaskService = Depends(get_task_servises)
+    ) -> TaskSchema:
+    try: 
+        return task_service.update_task(task_id=task_id, task_update=payload)
+    except TaskNotFound:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+
+@router.delete("/{task_id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_task(
+    task_id: str,
+    task_service: TaskService = Depends(get_task_servises)
+    ) -> None:
+    try:
+        return task_service.delete_task(task_id=task_id)
+    except TaskNotFound:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+
+
+
+"""
